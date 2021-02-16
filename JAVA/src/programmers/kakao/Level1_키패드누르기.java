02f00f0b75c03aed31835ec2ff44e937cb0d7b6e
Han-kiyeon <kylin3100@naver.com>
2021-02-16 19:48:08 +0900

    20210216 [kakao] 키패드 누르기 solve

--- JAVA/src/programmers/kakao/Level1_키패드누르기.java
+++ JAVA/src/programmers/kakao/Level1_키패드누르기.java
@@ -1,6 +1,8 @@
+package programmers.kakao;
+
 import java.awt.Point;
 
-class Solution {
+class Level1_키패드누르기 {
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] { 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 }, "right")); // LRLLLRLLRRL
 		System.out.println(solution(new int[] { 7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2 }, "left")); // LRLLRRLLLRR
@@ -8,51 +10,52 @@ class Solution {
 	}
 
 	public static String solution(int[] numbers, String hand) {
-		Point[] nums = new Point[10];
-		nums[0] = new Point(3, 1);
-		nums[1] = new Point(0, 0);
-		nums[2] = new Point(0, 1);
-		nums[3] = new Point(0, 2);
-		nums[4] = new Point(1, 0);
-		nums[5] = new Point(1, 1);
-		nums[6] = new Point(1, 2);
-		nums[7] = new Point(2, 0);
-		nums[8] = new Point(2, 1);
-		nums[9] = new Point(2, 2);
+		Point[] keypad = new Point[10];
+		keypad[0] = new Point(3, 1);
+		keypad[1] = new Point(0, 0);
+		keypad[2] = new Point(0, 1);
+		keypad[3] = new Point(0, 2);
+		keypad[4] = new Point(1, 0);
+		keypad[5] = new Point(1, 1);
+		keypad[6] = new Point(1, 2);
+		keypad[7] = new Point(2, 0);
+		keypad[8] = new Point(2, 1);
+		keypad[9] = new Point(2, 2);
+		
 		Point L = new Point(3, 0);
 		Point R = new Point(3, 2);
 
 		StringBuilder sb = new StringBuilder();
 		for (int num : numbers) {
 			if (num == 1 || num == 4 || num == 7) {
-				L.x = nums[num].x;
-				L.y = nums[num].y;
+				L.x = keypad[num].x;
+				L.y = keypad[num].y;
 				sb.append("L");
 			} else if (num == 3 || num == 6 || num == 9) {
-				R.x = nums[num].x;
-				R.y = nums[num].y;
+				R.x = keypad[num].x;
+				R.y = keypad[num].y;
 				sb.append("R");
 			} else {
-				int disL = Math.abs(L.x - nums[num].x) + Math.abs(L.y - nums[num].y);
-				int disR = Math.abs(R.x - nums[num].x) + Math.abs(R.y - nums[num].y);
+				int disL = Math.abs(L.x - keypad[num].x) + Math.abs(L.y - keypad[num].y);
+				int disR = Math.abs(R.x - keypad[num].x) + Math.abs(R.y - keypad[num].y);
 				if (disL == disR) {
 					if (hand.equals("right")) {
-						R.x = nums[num].x;
-						R.y = nums[num].y;
+						R.x = keypad[num].x;
+						R.y = keypad[num].y;
 						sb.append("R");
 					} else {
-						L.x = nums[num].x;
-						L.y = nums[num].y;
+						L.x = keypad[num].x;
+						L.y = keypad[num].y;
 						sb.append("L");
 					}
 				} else {
 					if (disR < disL) {
-						R.x = nums[num].x;
-						R.y = nums[num].y;
+						R.x = keypad[num].x;
+						R.y = keypad[num].y;
 						sb.append("R");
 					} else {
-						L.x = nums[num].x;
-						L.y = nums[num].y;
+						L.x = keypad[num].x;
+						L.y = keypad[num].y;
 						sb.append("L");
 					}
 				}
